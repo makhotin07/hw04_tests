af50b20d9195163fd06161b93073869a585aa301
Nikita Makhotin <makhotin.07@gmail.com>
2022-09-01 17:17:55 +0300

    1 rew 4 pep

--- yatube/posts/tests/test_views.py
+++ yatube/posts/tests/test_views.py
@@ -40,10 +40,14 @@ class PostsViewsTests(TestCase):
         # Шаблоны по адресам
         templates_page_names = {
             reverse('posts:index'): 'posts/index.html',
-            reverse('posts:group_list', kwargs={'slug': 'group-test-slug'}): 'posts/group_list.html',
-            reverse('posts:profile', kwargs={'username': 'auth'}): 'posts/profile.html',
-            reverse('posts:post_detail', kwargs={'post_id': 1}): 'posts/post_detail.html',
-            reverse('posts:post_edit', kwargs={'post_id': 1}): 'posts/create_post.html',
+            reverse('posts:group_list',
+                    kwargs={'slug': 'group-test-slug'}): 'posts/group_list.html',
+            reverse('posts:profile',
+                    kwargs={'username': 'auth'}): 'posts/profile.html',
+            reverse('posts:post_detail',
+                    kwargs={'post_id': 1}): 'posts/post_detail.html',
+            reverse('posts:post_edit',
+                    kwargs={'post_id': 1}): 'posts/create_post.html',
             reverse('posts:post_create'): 'posts/create_post.html',
 
         }
@@ -54,7 +58,8 @@ class PostsViewsTests(TestCase):
 
     def test_posts_create_correct_context(self):
         """Шаблон create сформирован с правильным контекстом и правильными полями формы."""
-        response = self.authorized_client.get(reverse('posts:post_create'))
+        response = self.authorized_client.get(
+            reverse('posts:post_create'))
 
         form_fields = {
             'text': forms.fields.CharField,
@@ -66,7 +71,8 @@ class PostsViewsTests(TestCase):
                 self.assertIsInstance(form_field, expected)
 
     def test_posts_edit_correct_context(self):
-        response = self.authorized_client.get(reverse('posts:post_edit', kwargs={'post_id': 1}))
+        response = self.authorized_client.get(
+            reverse('posts:post_edit', kwargs={'post_id': 1}))
         is_edit = response.context.get('is_edit')
         self.assertEqual(is_edit, True)
         form_fields = {
@@ -79,7 +85,8 @@ class PostsViewsTests(TestCase):
                 self.assertIsInstance(form_field, expected)
 
     def test_posts_post_detail_page_show_correct_context(self):
-        response = self.guest_client.get(reverse('posts:post_detail', kwargs={'post_id': 1}))
+        response = self.guest_client.get(
+            reverse('posts:post_detail', kwargs={'post_id': 1}))
         post_test = PostsViewsTests.post
 
         context_author = response.context['author']
@@ -100,19 +107,22 @@ class PostsViewsTests(TestCase):
         self.assertEqual(context_post, post_test)
 
     def test_posts_post_check_presence_group_page(self):
-        response = self.guest_client.get(reverse('posts:group_list', kwargs={'slug': 'group-test-slug'}))
+        response = self.guest_client.get(
+            reverse('posts:group_list', kwargs={'slug': 'group-test-slug'}))
         context_post = response.context['page_obj'][0]
         post_test = PostsViewsTests.post
         self.assertEqual(context_post, post_test)
 
     def test_posts_post_check_presence_profile_page(self):
-        response = self.guest_client.get(reverse('posts:profile', kwargs={'username': PostsViewsTests.user}))
+        response = self.guest_client.get(
+            reverse('posts:profile', kwargs={'username': PostsViewsTests.user}))
         context_post = response.context['page_obj'][0]
         post_test = PostsViewsTests.post
         self.assertEqual(context_post, post_test)
 
     def test_posts_post_check_not_presence_group_page(self):
-        response = self.guest_client.get(reverse('posts:post_detail', kwargs={'post_id': 1}))
+        response = self.guest_client.get(
+            reverse('posts:post_detail', kwargs={'post_id': 1}))
         context_post_detail = response.context['post_detail']
         post_test = PostsViewsTests.group
         self.assertEqual(context_post_detail.group, post_test)
@@ -126,7 +136,8 @@ class PaginatorViewsTest(TestCase):
 
     def test_posts_profile_page_show_correct_context(self):
         """Шаблон profile сформирован с правильным контекстом."""
-        response = self.guest_client.get(reverse('posts:profile', kwargs={'username': 'leo'}))
+        response = self.guest_client.get(
+            reverse('posts:profile', kwargs={'username': 'leo'}))
         context_author = response.context['author']
         context_count = response.context['count']
 
@@ -135,28 +146,33 @@ class PaginatorViewsTest(TestCase):
 
     def test_posts_profile_first_page_contains_ten_records(self):
         """Проверка: на profile leo: количество постов на первой странице равно 10."""
-        response = self.guest_client.get(reverse('posts:profile', kwargs={'username': 'leo'}))
+        response = self.guest_client.get(
+            reverse('posts:profile', kwargs={'username': 'leo'}))
         self.assertEqual(len(response.context['page_obj']), 10)
 
     def test_posts_profile_fourth_page_contains_three_records(self):
         """Проверка: на profile leo 4 странице должно быть 7 постов."""
-        response = self.guest_client.get(reverse('posts:profile', kwargs={'username': 'leo'}) + '?page=4')
+        response = self.guest_client.get(
+            reverse('posts:profile', kwargs={'username': 'leo'}) + '?page=4')
         self.assertEqual(len(response.context['page_obj']), 7)
 
     def test_posts_index_first_page_contains_ten_records(self):
         """ Проверка: на index: количество постов на первой странице равно 10."""
-        response = self.guest_client.get(reverse('posts:index'))
+        response = self.guest_client.get(
+            reverse('posts:index'))
         self.assertEqual(len(response.context['page_obj']), 10)
 
     def test_posts_index_fourth_page_contains_three_records(self):
         """  Проверка: на index, 4 странице должно быть 7 постов."""
-        response = self.guest_client.get(reverse('posts:index') + '?page=4')
+        response = self.guest_client.get(
+            reverse('posts:index') + '?page=4')
         self.assertEqual(len(response.context['page_obj']), 7)
 
     def test_posts_group_posts_page_show_correct_context(self):
         """  Проверка: на group_list: правильный контекст"""
 
-        response = self.guest_client.get(reverse('posts:group_list', kwargs={'slug': 'third_group'}))
+        response = self.guest_client.get(
+            reverse('posts:group_list', kwargs={'slug': 'third_group'}))
         context_group = response.context['group']
 
         self.assertEqual(str(context_group), 'Третья группа')
